8b5090df213f594ec9d39d339b9b773e24ab2de7
Naoto Ida <naoto.ida+programming@gmail.com>
2015-11-06 23:37:15 +0900

    First commit

---
+++ LineClient.php
@@ -0,0 +1,39 @@
+<?php
+class LineClient
+{
+  private $token;
+  private $BASE_API_URL = "https://api.line.me/v1/";
+
+  private function getToken() {
+    return $this->token;
+  }
+
+  public function setToken($newToken) {
+    $this->token = $newToken;
+  }
+
+  private function request($params = null) {
+    $headers = [
+      "Content-Type: application/json",
+      "Authorization: Bearer ".$this->token
+    ];
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_URL, $this->BASE_API_URL."profile");
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
+    
+    $res = curl_exec($c);
+    curl_close($c);
+    $res = json_decode($res);
+    foreach ($res as $key => $value) {
+      $arr[$key] = $value;
+    }
+    return $arr;
+  }
+
+  public function getUserProfile() {
+    return $this->request();
+  }
+
+}
+?>
